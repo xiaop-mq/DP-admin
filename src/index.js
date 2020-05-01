e348f1d032d27a9fbf822983cd9c30cd595c940c
pppmmmqqq <2674264426@qq.com>
2020-05-01 22:52:37 +0800

    DP后台项目介绍

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
+// 入口 js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+// import 'antd/dist/antd.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 将app组件标签渲染到index页面的div上
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Admin extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Admin</div>
+         );
+    }
+}
+ 
+export default Admin;
